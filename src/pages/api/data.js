2c5de39d363aba8cfe18e13509e529dab88162cd
Bryan Navarrete <bnavarrete@dazzsoft.com>
2024-07-17 22:30:30 -0500

    creacion de mock para la consulta de usuarios

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,5 +1,5 @@
 // pages/api/data.js
-import { query } from '../../lib/db';
+import { query } from '@lib/db';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
@@ -8,8 +8,20 @@ export default async function handler(req, res) {
   }
 
   try {
-    const result = await query('SELECT * FROM your_table');
-    res.status(200).json(result.rows);
+    // const result = await query('SELECT * FROM users');
+    // res.status(200).json(result.rows);
+    
+    const rows = [
+      {"id": 1, "name": "Bryan Navarrete"},
+      {"id": 2, "name": "Erick Gilbert"},
+      {"id": 3, "name": "Yailing Funsang"},
+      {"id": 4, "name": "Catalina Rivas"},
+      {"id": 5, "name": "Stalyn Aviles"},
+      {"id": 6, "name": "John Navarrete"},
+      {"id": 7, "name": "Alexis Palate"}
+    ]
+
+    res.status(200).json(rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error' });
